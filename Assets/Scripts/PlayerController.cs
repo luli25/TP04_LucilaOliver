34e346a26205eb66fb35837d43642b40ff08c31b
LAPTOP-EFS5H6SO\lucil <lucilaoliver8@gmail.com>
2024-10-07 20:17:08 -0300

    Player jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float jumpForce = 10f;
+
+    [SerializeField]
+    private Animator animator;
+
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("onJump");
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
