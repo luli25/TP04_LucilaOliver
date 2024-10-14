8db6b603f5f3aabe9f627a8b69c8c16f2362fbe7
LAPTOP-EFS5H6SO\lucil <lucilaoliver8@gmail.com>
2024-10-14 16:07:46 -0300

    Added script for collectibles

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Collectibles : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+
+    private float moveSpeed = 5f;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        rb.velocity = Vector2.left * moveSpeed;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Destroy(this);
+            Debug.Log("Objeto destruido");
+        }
+    }
+
 }
