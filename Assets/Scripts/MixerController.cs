d14054d058df15b81e36b9dfad1c35aa3a8b7c48
LAPTOP-EFS5H6SO\lucil <lucilaoliver8@gmail.com>
2024-12-17 11:27:37 -0300

    Added audiomixer and audiomixer controller

---
+++ Assets/Scripts/MixerController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixerController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer mixer;
+
+    public void SetVolume(float sliderValue)
+    {
+        mixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+    }
+}
