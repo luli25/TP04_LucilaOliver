c8f1cc8374253343ac819a59e90db69742649388
LAPTOP-EFS5H6SO\lucil <lucilaoliver8@gmail.com>
2025-01-13 14:48:25 -0300

    Finished main menu UI
    -Now master and fx volumes can be change with sliders
    -Added functionality to back button

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -13,6 +13,23 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField]
     private Slider fxSlider;
 
+    [SerializeField]
+    private Slider masterSlider;
+
+    [SerializeField]
+    private Button back;
+
+    [SerializeField]
+    private GameObject menuPanel;
+
+    [SerializeField]
+    private GameObject settingsPanel;
+
+    private void Awake()
+    {
+        back.onClick.AddListener(OnBackButtonClicked);
+    }
+
     private void Start()
     {
         SetMusicVolume();
@@ -28,5 +45,25 @@ public class VolumeSettings : MonoBehaviour
     {
         float fxVolume = fxSlider.value;
         mixer.SetFloat("fx", Mathf.Log10(fxVolume) * 20);
+    }
+
+    public void SetMasterVolume()
+    {
+        float masterVolume = masterSlider.value;
+        mixer.SetFloat("master", Mathf.Log10(masterVolume) * 20);
+    }
+
+    private void OnDestroy()
+    {
+       back.onClick.RemoveAllListeners();
+    }
+
+    private void OnBackButtonClicked()
+    {
+        if (!menuPanel.activeSelf)
+        {
+            menuPanel.SetActive(true);
+            settingsPanel.SetActive(false);
+        }
     }
 }
